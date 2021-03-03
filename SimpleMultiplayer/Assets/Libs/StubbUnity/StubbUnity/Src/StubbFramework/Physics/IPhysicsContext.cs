00a9b44b18a83a445f0f209c69300129439c6d9f
VirtualMaestro <virtualmaestro@gmail.com>
2021-03-03 18:40:01 +0100

    remove prev Stubb

--- SimpleMultiplayer/Assets/Libs/StubbUnity/StubbUnity/Src/StubbFramework/Physics/IPhysicsContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StubbUnity.StubbFramework.Physics
-{
-    /// <summary>
-    /// Context which is implemented this interface will be processed only on PhysicsUpdate loop (e.g. for Unity FixedUpdate).
-    /// </summary>
-    public interface IPhysicsContext : IStubbContext
-    {
-    }
-}
